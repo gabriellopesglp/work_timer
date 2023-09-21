5c0ccad262577fa97d552d1a21f3eb3bdb87deee
Gabriel Lopes <gabriel.lopes_glp@outlook.com>
2023-09-21 17:55:51 -0300

    deploy config gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/work_timer',
   server: {
     port: 3000,
   },

b7eafb59f64e193e4d07a766f30b2bc2ba643d9d
Gabriel Lopes <gabriel.lopes_glp@outlook.com>
2023-09-21 18:03:06 -0300

    deploy config gh-pages router

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,10 @@ import { Home } from './pages/Home/index'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
+      {/* work_timer (github pages deploy) */}
+      <Route path="/work_timer" element={<DefaultLayout />}>
+        <Route path="/work_timer" element={<Home />} />
+        <Route path="/work_timer/history" element={<History />} />
       </Route>
     </Routes>
   )
